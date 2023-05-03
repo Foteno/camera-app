d80050b2c26b592f00cb9dbe8d4415ff6ec6797b
Foteno <patrikass32@gmail.com>
2023-05-03 20:59:12 +0300

    Initial commit with camera
    Added camera and extracted components, ready for development

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,88 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import { useIsFocused } from '@react-navigation/native';
+import { Camera, CameraCapturedPicture } from 'expo-camera';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import CameraPreview from '../../components/CameraPreview';
+import CameraWrapper from '../../components/CameraWrapper';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+  const [permission, setPermission] = Camera.useCameraPermissions()
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [capturedImage, setCapturedImage] = useState<CameraCapturedPicture>()
+  const [cameraReady, setCameraReady] = useState(false)
+
+  let camera: Camera | null = null
+  const focused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+    })();
+  }, []);
+
+  function __setCamera(cameraToSet: Camera | null): void {
+    camera = cameraToSet
+  }
+
+  function __retakePicture(): void {
+    setCapturedImage(undefined)
+    setPreviewVisible(false)
+  }
+
+  async function __takePicture() {
+    if (!camera) return
+    if (!cameraReady) return
+    const photo: CameraCapturedPicture = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+
+  if (permission) {
+    if (previewVisible && capturedImage) {
+      return (
+        <CameraPreview photo={capturedImage} __retakePicture={__retakePicture} __sendPhoto={() => { }} />
+      )
+    } else if (focused) {
+      return (
+        <CameraWrapper __setCameraCallback={__setCamera} __setCameraReady={setCameraReady} __takePicture={__takePicture}/>
+      )
+    }
+  }
+
+  if (!permission) {
+    return (
+      <View style={permissionStyles.container}>
+        <TouchableOpacity onPress={() => { }}
+          style={permissionStyles.button}>
+
+          <Text style={permissionStyles.buttonText}>Take picture</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
+const permissionStyles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#fff',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  title: {
-    fontSize: 20,
+  button: {
+    width: 130,
+    borderRadius: 4,
+    backgroundColor: '#14274e',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40
+  },
+  buttonText: {
+    color: '#fff',
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    textAlign: 'center'
+  }
 });
---
+++ components/CameraWrapper.tsx
@@ -0,0 +1,67 @@
+import { Camera, CameraType } from "expo-camera";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+type CameraWrapperProps = {
+    __setCameraCallback: (ref: Camera | null) => void,
+    __setCameraReady: (ready: boolean) => void,
+    __takePicture: () => void
+}
+
+export default function CameraPreview (props: CameraWrapperProps) {
+  return (
+    <Camera
+      style={cameraStyles.camera}
+      type={CameraType.back}
+      ratio="16:9"
+      useCamera2Api={true}
+      ref={(ref) => props.__setCameraCallback(ref)}
+      onCameraReady={() => props.__setCameraReady(true)}>
+      <View style={cameraStyles.takePictureBar}>
+        <View style={cameraStyles.takePictureButtonContainer}>
+          <TouchableOpacity
+            onPress={props.__takePicture}
+            style={cameraStyles.takePictureButton} />
+        </View>
+      </View>
+    </Camera>
+  )
+} 
+
+const cameraStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center'
+  },
+  camera: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%'
+  },
+  takePictureBar: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    flex: 1,
+    padding: 10,
+  },
+  takePictureButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 20,
+    backgroundColor: '#eee'
+  },
+  takePictureButtonContainer: {
+    flex: 1,
+    alignItems: 'center'
+  }
+});
